58e7e22045cf74014ee0ed3a435d94ba2d8b6aa9
LalithK90 <asakahatapitiya@gmail.com>
2020-12-12 14:37:57 +0530

    Invoice backend part

--- src/main/java/lk/samarasingherSuper/asset/invoice/entity/InvoiceItem.java
+++ src/main/java/lk/samarasingherSuper/asset/invoice/entity/InvoiceItem.java
@@ -1,26 +1,35 @@
 package lk.samarasingherSuper.asset.invoice.entity;
 
+
 import com.fasterxml.jackson.annotation.JsonFilter;
+import lk.samarasingherSuper.asset.item.entity.Item;
 import lk.samarasingherSuper.util.audit.AuditEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonFilter("InvoiceItemQuantity")
-public class InvoiceItemQuantity extends AuditEntity {
+@JsonFilter("InvoiceItem")
+public class InvoiceItem extends AuditEntity {
+
+    @Column(nullable = false)
     private String quantity;
 
+    @Column( nullable = false, precision = 10, scale = 2 )
+    private BigDecimal sellPrice;
+
     @ManyToOne
-    private ItemBatch itemBatch;
+    private Item item;
 
     @ManyToOne
     private Invoice invoice;
