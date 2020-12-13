cebb2014dbf0902b21a5f1c3a11e1b2825e67165
LalithK90 <asakahatapitiya@gmail.com>
2020-12-13 20:51:51 +0530

    Invoice Item active and inactive was modified

--- src/main/java/lk/samarasingher_super/asset/invoice_item/entity/InvoiceItem.java
+++ src/main/java/lk/samarasingher_super/asset/invoice_item/entity/InvoiceItem.java
@@ -2,6 +2,7 @@ package lk.samarasingher_super.asset.invoice_item.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import lk.samarasingher_super.asset.common_asset.model.enums.ActiveOrInactive;
 import lk.samarasingher_super.asset.invoice.entity.Invoice;
 import lk.samarasingher_super.asset.item.entity.Item;
 import lk.samarasingher_super.util.audit.AuditEntity;
@@ -10,9 +11,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -28,6 +27,9 @@ public class InvoiceItem extends AuditEntity {
 
     @Column( nullable = false, precision = 10, scale = 2 )
     private BigDecimal sellPrice;
+
+    @Enumerated( EnumType.STRING)
+    private ActiveOrInactive activeOrInactive;
 
     @ManyToOne
     private Item item;
