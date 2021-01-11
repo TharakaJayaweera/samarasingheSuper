c240aef9d048d1197d22780890ba6c11f43ac292
LalithK90 <asakahatapitiya@gmail.com>
2021-01-11 22:48:09 +0530

    Security configuration was added

--- src/main/java/lk/samarasingher_super/configuration/SecurityConfig.java
+++ src/main/java/lk/samarasingher_super/configuration/SecurityConfig.java
@@ -24,19 +24,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final String[] ALL_PERMIT_URL = {"/favicon.ico", "/img/**", "/css/**", "/js/**", "/webjars/**",
       "/login", "/select/**", "/", "/index"};
-//private final String[] ADMIN = {"/category/**", "/customer/**", "/employee/**", "/goodReceivedNote/**",
-//      "/invoice/**",
-//      "/item/**", "/purchaseOrder/**", "/role/**", "/supplier/**", "/supplierItem/**", "/user/**"};
-/* private final String[] MANAGER = {"/category/**", "/customer/**", "/discountRatio/**", "/employee/**",
-      "/goodReceivedNote/**", "/invoice/**",
-      " /item/**", "/ledger/**", "/payment/**", "/purchaseOrder/**", "/role/**", "/supplier/**", "/supplierItem/**",
-      "/user/**"};
-  private final String[] PROCUMENT_MANAGER = {"/category/**", "/goodReceivedNote/**", "/invoice/**", " /item/**",
-      "ledger/**", "/purchaseOrder/**",
+private final String[] ADMIN = { "/role/**", "/user/**","/employee/save","/employee/search","/employee/{id}"};
+ private final String[] MANAGER = {"/report/manager/**"};
+  private final String[] PROCUREMENT_MANAGER = {"/category/**", "/goodReceivedNote/**", " /item/**",
+      "/ledger/**", "/purchaseOrder/**",
       "/supplier/**", "/supplierItem/**"};
-  private final String[] ACCOUNT_MANAGER = {"/payment/**"};
+  private final String[] ACCOUNT_MANAGER = {"/payment/**", "/invoice/**"};
   private final String[] HR_MANAGER = {"/employee/**"};
-  private final String[] CASHIER = {"/category/getCategory/**", "/invoice/add", "/brand/**", "/itemColor/**"};*/
+  private final String[] CASHIER = {"/category/getCategory/**", "/invoice/add", "/ledger"};
 
   @Bean
   public UserDetailsServiceImpl userDetailsService() {
@@ -98,13 +93,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 //to see actuator details
                 .antMatchers(ALL_PERMIT_URL).permitAll()
                 //this is used the normal admin to give access every url mapping
-                //.antMatchers(ADMIN).hasAnyRole("ADMIN")
+                .antMatchers(ADMIN).hasAnyRole("ADMIN")
                 //Need to login for access those are
-           /*     .antMatchers(MANAGER).hasAnyRole("MANAGER")
-                .antMatchers(PROCUMENT_MANAGER).hasAnyRole("PROCUMENT_MANAGER")
+                .antMatchers(MANAGER).hasAnyRole("MANAGER")
+                .antMatchers(PROCUREMENT_MANAGER).hasAnyRole("PROCUMENT_MANAGER")
                 .antMatchers(ACCOUNT_MANAGER).hasAnyRole("ACCOUNT_MANAGER")
                 .antMatchers(HR_MANAGER).hasAnyRole("HR_MANAGER")
-                .antMatchers(CASHIER).hasAnyRole("CASHIER")*/
+                .antMatchers(CASHIER).hasAnyRole("CASHIER")
                 .anyRequest()
                 .authenticated())
         // Login form
